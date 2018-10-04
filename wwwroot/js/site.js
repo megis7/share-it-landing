be7c207d8c8775084d521e9233cae2f965cb8025
Michalis <megisoglou56@gmail.com>
2018-10-04 10:52:26 +0300

    Added landscape/portrait images - font changes.

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,12 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(function() {
+  return $(".carousel.lazy").on("slide", function(ev) {
+      console.log("hello")
+    var lazy;
+    lazy = $(ev.relatedTarget).find("img[data-src]");
+    lazy.attr("src", lazy.data('src'));
+    lazy.removeAttr("data-src");
+  });
+});
